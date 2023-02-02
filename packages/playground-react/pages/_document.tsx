cf803c50de9005ef47bdf4c91367722865386b01
Chris LaRocque <larocque.christopher@gmail.com>
2023-02-01 21:20:21 -0500

    Remove nextjs warning when viewport meta is in _document.js

--- packages/playground-react/pages/_document.tsx
+++ packages/playground-react/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html>
       <Head>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 
